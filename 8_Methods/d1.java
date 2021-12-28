9800cae271b4b7c0f06954bfa328b5fc4cb07544
Sagar Kumar <19gietcse003@gmail.com>
2021-12-28 19:12:00 +0530

    methods start

---
+++ 8_Methods/d1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class d1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 15, b = 10, c;
+        c = max(a,b);
+        System.out.println(c);
+        sc.close();
+    }
+    static int max(int x, int y){
+        if(x>y)
+            return x;
+         else
+            return y;
+        
+    }
+}
